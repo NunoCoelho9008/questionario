e630cbebee7a24e4862a18ec51ec70618b2c66b1
Nuno Santos <134955741+NunoCoelho9008@users.noreply.github.com>
2024-04-22 10:18:14 +0100

    Ficheiros adicionados

---
+++ submit.php
@@ -0,0 +1,33 @@
+<?php
+// Establish a connection to the database
+$servername = "localhost";
+$username = "id22054120_termoave";
+$password = "";
+$dbname = "id22054120_termoavedb";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Retrieve the form data
+$campo1 = $_POST['campo1'];
+$campo2 = $_POST['campo2'];
+$campo3 = $_POST['campo3'];
+$campo4 = $_POST['campo4'];
+$campo5 = $_POST['campo5'];
+
+// Insert the form data into the TERMOAVE table
+$sql = "INSERT INTO TERMOAVE (campo1, campo2, campo3, campo4, campo5) VALUES ('$campo1', '$campo2', '$campo3', '$campo4', '$campo5')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Data inserted successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Close the database connection
+$conn->close();
+?>
